6ab8e649147db83543075f677b7d3009c990dbe3
youarebest119 <80273689+youarebest119@users.noreply.github.com>
2024-10-04 00:15:45 +0530

    fix: ' changed to entity'

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -7,7 +7,7 @@ const Contact = () => {
         <div className="contact">
             <h4 className="custom_badge">Contact</h4>
             <h2>Get In Touch</h2>
-            <p>Feel free to drop me a direct message with any questions, and I'll get back to you when I can. Please note that I might not respond to every message.</p>
+            <p>Feel free to drop me a direct message with any questions, and I&apos;ll get back to you when I can. Please note that I might not respond to every message.</p>
             <Button>Message <MessageIcon /></Button>
         </div>
     )
